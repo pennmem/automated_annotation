27d9953b9e94facfd01d58a1c1406afc7290615b
Riley DeHaan <dashags123456789@gmail.com>
2024-07-08 22:55:42 -0400

    update environment.yml

--- test_whisperx.py
+++ test_whisperx.py
@@ -13,15 +13,16 @@ use_gpu = False
 device = "cuda:0" if use_gpu else "cpu"
 vad = load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=None, model_fp=None)
 in_dir = 'dependencies/annotation_gauntlet/session_0'
-# with open('input_dirs_splits.pkl', 'rb') as f:
-#     input_dirs = pickle.load(f)
-# in_dir = input_dirs['train'][0]
+
+output_dir = 'test/whisperx_vad_fun/audio/'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # save out .wav files to .npz files since .wav won't load in Jupyter Lab
 for wav in glob.glob(in_dir + '/*.wav'):
     print(wav)
     audio = whisperx.load_audio(wav)
-    with open('test/whisperx_vad_fun/audio/' + os.path.split(wav)[-1].split('.')[0] + '.npy', 'wb') as f:
+    with open(output_dir + os.path.split(wav)[-1].split('.')[0] + '.npy', 'wb') as f:
         np.save(f, audio)
     
     pred = vad({"waveform": torch.from_numpy(audio).unsqueeze(0), "sample_rate": SAMPLE_RATE})
